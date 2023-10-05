aa8f04cbdea5452dbca5e613ea38dda9f46eec13
j3ssie <j3ssiejjj@gmail.com>
2023-10-05 18:55:19 +0700

    Updating binaries

--- data/scripts/install-external-binaries.sh
+++ data/scripts/install-external-binaries.sh
@@ -63,7 +63,7 @@ download $TMP_DIST/gowitness https://github.com/sensepost/gowitness/releases/dow
 cp $TMP_DIST/gowitness $BINARIES_PATH/gowitness
 
 install_banner "trufflehog"
-download $TMP_DIST/trufflehog.gz https://github.com/trufflesecurity/trufflehog/releases/download/v3.57.0/trufflehog_3.57.0_linux_amd64.tar.gz
+download $TMP_DIST/trufflehog.gz https://github.com/trufflesecurity/trufflehog/releases/download/v3.59.0/trufflehog_3.59.0_linux_amd64.tar.gz
 extractGz $TMP_DIST/trufflehog.gz
 
 install_banner "gitleaks"
